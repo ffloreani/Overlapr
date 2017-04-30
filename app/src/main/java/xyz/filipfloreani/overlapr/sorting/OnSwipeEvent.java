7658a8283aeeac24193c69a38dad8a873be07474
Filip Floreani <floreani.filip@gmail.com>
2017-04-30 22:39:44 +0200

    Last commit failed miserably, this one ought to do it

--- app/src/main/java/xyz/filipfloreani/overlapr/sorting/OnSwipeEvent.java
+++ app/src/main/java/xyz/filipfloreani/overlapr/sorting/OnSwipeEvent.java
@@ -1,8 +1,10 @@
 package xyz.filipfloreani.overlapr.sorting;
 
+import android.view.View;
+
 /**
  * Created by filipfloreani on 30/04/2017.
  */
-
 public interface OnSwipeEvent {
+    void swipeEventDetected(View v, SwipeListener.SwipeDirectionEnum swipeDirection);
 }
