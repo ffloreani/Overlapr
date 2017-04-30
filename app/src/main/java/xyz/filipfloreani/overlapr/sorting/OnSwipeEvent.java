55e6d1e2d01e7d2dff8eda54b499c71daf01aef6
Filip Floreani <floreani.filip@gmail.com>
2017-04-30 22:34:12 +0200

    Created a simple swipe listener and an on-swipe interface for swipe events

---
+++ app/src/main/java/xyz/filipfloreani/overlapr/sorting/OnSwipeEvent.java
@@ -0,0 +1,8 @@
+package xyz.filipfloreani.overlapr.sorting;
+
+/**
+ * Created by filipfloreani on 30/04/2017.
+ */
+
+public interface OnSwipeEvent {
+}
---
+++ app/src/main/java/xyz/filipfloreani/overlapr/sorting/SwipeListener.java
@@ -0,0 +1,10 @@
+package xyz.filipfloreani.overlapr.sorting;
+
+import View.OnTouchListener;
+
+/**
+ * Created by filipfloreani on 30/04/2017.
+ */
+
+public class SwipeListener implements OnTouchListener {
+}
